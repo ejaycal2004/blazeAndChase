7b9ac7f2ab8b1580597b75c7844a2c5035510655
Elijah Calip <ecalip2022@208-Engineering-4.local>
2019-04-11 13:26:17 -0700

    Initial Commit

---
+++ Blaze and ChaseTests/Blaze_and_ChaseTests.swift
@@ -0,0 +1,34 @@
+//
+//  Blaze_and_ChaseTests.swift
+//  Blaze and ChaseTests
+//
+//  Created by Elijah Calip on 4/11/19.
+//  Copyright © 2019 Elijah Calip. All rights reserved.
+//
+
+import XCTest
+@testable import Blaze_and_Chase
+
+class Blaze_and_ChaseTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
